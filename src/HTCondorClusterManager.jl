7245492588e44551bf95019cb1ae168abdf6645b
Dilum Aluthge <dilum@aluthge.com>
2025-02-16 07:55:06 -0500

    Excision: Remove all non-Condor code

--- src/HTCondorClusterManager.jl
+++ src/HTCondorClusterManager.jl
@@ -1,23 +1,17 @@
 module HTCondorClusterManager
 
-using Distributed
-using Sockets
-using Pkg
+import Distributed
+import Sockets
+import Pkg
+
+using Distributed: launch, manage, kill, init_worker, connect
 
 export launch, manage, kill, init_worker, connect
-import Distributed: launch, manage, kill, init_worker, connect
+
 
 worker_cookie() = begin Distributed.init_multi(); cluster_cookie() end
 worker_arg() = `--worker=$(worker_cookie())`
 
-
-# PBS doesn't have the same semantics as SGE wrt to file accumulate,
-# a different solution will have to be found
-include("qsub.jl")
-include("scyld.jl")
 include("condor.jl")
-include("slurm.jl")
-include("affinity.jl")
-include("elastic.jl")
 
 end
